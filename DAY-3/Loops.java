3d449c1234b06b3a073cd056a4f5c76de16b03a7
Khushi Khandelwal <khushikhandelwal071@gmail.com>
2026-07-25 19:16:03 +0530

    add project files

---
+++ DAY-3/Loops.java
@@ -0,0 +1,12 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class Loops {
+   public Loops() {
+   }
+
+   public static void main(String[] var0) {
+      for(int var1 = 0; var1 < 100; ++var1) {
+         System.out.println("Hello World");
+      }
+
+   }
+}
---
+++ DAY-3/Whileloop.java
@@ -0,0 +1,10 @@
+public class Whileloop {
+   public static void main(String[] args) {
+    int i = 0;
+    while(i<20)
+    {
+    System.out.println(i);
+    i++; // Incrementing the value of i by 1
+    }
+}
+}

0c15206d53133b2e56ea9a294b25d135f2d0d243
Khushi Khandelwal <khushikhandelwal071@gmail.com>
2026-08-21 19:08:35 +0530

    add project files

---
+++ DAY-11/SelectionSorting.java
@@ -0,0 +1,28 @@
+public class SelectionSorting {
+    public static void printArray(int[] arr){
+    for(int i = 0; i<arr.length;i++){
+    System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+    int[] arr = {7,8,3,1,2};
+
+    // TIME COMPLEXITY = O(n^2)
+
+    // SELECTION SORT
+    for(int i=0; i<arr.length-1; i++){
+    int smallest = i;
+    for(int j=i+1; j<arr.length; j++){
+    if(arr[j] < arr[smallest]){
+    smallest = j;
+    }
+    }
+    // SWAP
+    int temp = arr[smallest];       
+    arr[smallest] = arr[i];
+    arr[i] = temp;
+}
+printArray(arr);
+}
+}
+
